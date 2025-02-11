bd2b84e944c3804e78cc2f26e789ff455c8c366e
jayveehidlao11 <hidlaojayvee18@gmail.com>
2025-02-11 23:53:08 +0800

    portfolio.1.1

--- js/responsive.js
+++ js/responsive.js
@@ -1,1 +1,23 @@
-const burger = document.getElementById('burger-icon');
+let burger = document.getElementById('burger');
+const sidebar = document.getElementById("sidebar");
+const viewportWidth = window.innerWidth;
+
+
+burger.addEventListener('click', () => {
+    if(sidebar.style.width == ''){
+       
+        
+        sidebar.style.width = (viewportWidth <='339' ? '100%' : '200px'); 
+    }else if(sidebar.style.width == '200px' || sidebar.style.width == '100%' ){
+        sidebar.style.width = (viewportWidth <='339' ? '0px' : '0px'); 
+    }
+    else{
+        sidebar.style.width = (viewportWidth <='339' ? '100%' : '200px'); 
+    }
+    console.log(viewportWidth);
+   console.log(sidebar.style.width);
+   
+    
+});
+
+  
